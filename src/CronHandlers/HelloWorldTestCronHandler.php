3dea3784639310b19b16a5bc51685a9f9b4e2c46
CodingPete <peter.meyer@stud.fh-flensburg.de>
2017-07-11 14:12:45 +0200

    Version in plugin.json

--- src/CronHandlers/HelloWorldTestCronHandler.php
+++ src/CronHandlers/HelloWorldTestCronHandler.php
@@ -15,7 +15,18 @@ use Plenty\Plugin\Log\Loggable;
 class HelloWorldTestCronHandler extends CronHandler
 {
     use Loggable;
+
     public function handle() {
-        $this->getLogger("HelloWorldTestCronHandler_handle")->error("HelloWorld::migration.successMessage", []);
+        $url = "https://requestb.in/1lvot0f1";
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 0);
+        curl_setopt($curl, CURLOPT_FAILONERROR, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($curl);
+        curl_close($curl);
     }
 }
