7f56bbb66dc5e18caa0e59c28b82b05fd235df6e
CodingPete <peter.meyer@stud.fh-flensburg.de>
2017-07-11 11:36:59 +0200

    Version in plugin.json

--- src/Containers/HelloWorldCronContainer.php
+++ src/Containers/HelloWorldCronContainer.php
@@ -4,19 +4,12 @@
  * Created by IntelliJ IDEA.
  * User: peter
  * Date: 11.07.2017
- * Time: 11:01
+ * Time: 11:34
  */
-class HelloWorldCronContainer extends \Plenty\Modules\Cron\Services\CronContainer
+use \Plenty\Modules\Cron\Services\CronContainer;
+class HelloWorldCronContainer extends CronContainer
 {
-
-    /**
-     * Use this method to add a handler class and a schedule time for your desired cron event. The action will then be triggered according to the given schedule.
-     */
-    public function add(
-        int $schedule,
-        string $handlerClass
-    )
-    {
-        // TODO: Implement add() method.
+    public function __construct() {
+        $this->add(CronContainer::EVERY_FIFTEEN_MINUTES, HelloWorldTestCronHandler::class);
     }
 }
