74fbc0ab722406c96dc8cc54f114d9c6ee023d07
CodingPete <peter.meyer@stud.fh-flensburg.de>
2017-07-11 14:40:11 +0200

    Version in plugin.json

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -2,6 +2,7 @@
 namespace HelloWorld\Controllers;
 
 use Plenty\Plugin\Controller;
+use Plenty\Plugin\Log\Loggable;
 use Plenty\Plugin\Templates\Twig;
 
 /**
@@ -10,12 +11,14 @@ use Plenty\Plugin\Templates\Twig;
  */
 class ContentController extends Controller
 {
+    use Loggable;
 	/**
 	 * @param Twig $twig
 	 * @return string
 	 */
 	public function sayHello(Twig $twig):string
 	{
+	    $this->getLogger("ContentController_sayHello")->debug("Hallo Welt!");
 		return $twig->render('HelloWorld::content.hello');
 	}
 }
