97bc9789ea840f4a769e0e6a16e266ccb3ea06d9
CodingPete <peter.meyer@stud.fh-flensburg.de>
2017-07-11 13:41:27 +0200

    Version in plugin.json

--- src/CronHandlers/HelloWorldTestCronHandler.php
+++ src/CronHandlers/HelloWorldTestCronHandler.php
@@ -8,11 +8,14 @@
  */
 namespace HelloWorld\CronHandlers;
 
+use Plenty\Log\Models\Log;
 use \Plenty\Modules\Cron\Contracts\CronHandler;
+use Plenty\Plugin\Log\Loggable;
 
 class HelloWorldTestCronHandler extends CronHandler
 {
+    use Loggable;
     public function handle() {
-
+        $this->getLogger("Cron")->debug("Hallo Welt");
     }
 }
